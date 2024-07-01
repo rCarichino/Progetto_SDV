3a37d48614cbaf6dcfd8c42c5ac65c0add0a7933
davidegrazioso <d.grazioso@studenti.uniba.it>
2024-07-01 23:24:40 +0200

    aggiustato il door os

--- scena1/Node2D.gd
+++ scena1/Node2D.gd
@@ -5,3 +5,11 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+
+
+func _on_door_mouse_exited():
+	pass # Replace with function body.
+
+
+func _on_Si_mouse_exited():
+	pass # Replace with function body.
