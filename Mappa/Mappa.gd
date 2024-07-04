b77fa60b4d3063d5d18f32e2f63bda1f2474be3b
MattiaDiLeo <m.dileo56@studenti.uniba.it>
2024-07-04 18:30:58 +0200

    Modifiche nome cartelle, aggiunto browser, aggiunti file txt in Cartella Appunti, modificato il cursore ed il font

---
+++ Mappa/Mappa.gd
@@ -0,0 +1,32 @@
+extends TextureButton
+#funzioni per il doppio click e il cambio cursore (da copiare per le altre icone)
+signal double_click
+
+var double_speed: int = 1000*0.25
+var last_click: = 0.0
+var selected = false
+var of = Vector2(0,0)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	connect("pressed",self,"on_pressed")
+	connect("double_click",self, "on_double_click")
+
+func double_click_detect():
+		var new_click: = OS.get_ticks_msec()
+		if new_click - last_click <= double_speed:
+				return true
+		last_click = new_click
+		return false
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+
+func _on_impostazioni_pressed():
+	if double_click_detect():
+		get_tree().change_scene("res://Mappa/Mappa.tscn")
+		
+
+
