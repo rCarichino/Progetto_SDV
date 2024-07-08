8478cdf838a2433b76e550592c9a7e1018763806
rCarichino <riccardo.carichino@gmail.com>
2024-07-08 12:38:40 +0200

    Aggiunta Chat
    Aggiunta la prima bozza della chat, attualmente solo la chat di Alessia é """"funzionante""""

---
+++ Global.gd
@@ -0,0 +1,29 @@
+extends Node
+
+var unknown_unloked = false
+
+var chat_messages = []
+
+func add_message(message):
+	chat_messages.append(message)
+
+func get_messages():
+	return chat_messages
+
+func modify_unknown():
+	unknown_unloked = !unknown_unloked
+
+func get_unknowed():
+	return unknown_unloked
+
+var answerCarlo = [
+	"Ehy Jimmy, cm va? ma l’hai comprato\n alla fine il nuovo CoD",
+	"Sì anche io, prime impressioni?",
+	"A me sta piacendo un sacco",
+	"K fai?",
+	"Bro, nn faccio nulla tt il giorno cm sempre xD",
+	"Cmq, tua sorella cm sta? :P",
+	"??",
+	"nn ero io, cmq ora stacco e vado a mangiare"
+]
+
