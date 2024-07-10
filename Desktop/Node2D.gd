5655c26eff77aeee7aa462888b51483b6f5ce93e
FraBernardi <fran.bern.02@gmail.com>
2024-07-10 17:07:35 +0200

    implementata parte della timeline centro -> residence

--- Desktop/Node2D.gd
+++ Desktop/Node2D.gd
@@ -3,8 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
-	while(true):
+
 		if(Global.chat_completed == true):
 			seleziona_chiamata(Global.stato_chiamata)
 	
@@ -19,9 +18,32 @@ func seleziona_chiamata(stato):
 			var second_dialog = Dialogic.start('chiamata2')
 			add_child(second_dialog)
 			second_dialog.connect("timeline_end", self, 'after_chiamata2')
+			second_dialog.connect("dialogic_signal",self, 'signal_chiamata2')
 			
 		elif(stato == 3):
 			pass
+		
+		elif(stato == 4):
+			var fourth_dialog = Dialogic.start('Scelta3_CentroSportivo')
+			add_child(fourth_dialog)
+			fourth_dialog.connect("dialogic_signal",self, 'signal_centrosportivo')
+			
+		
+		##scelta prefabbricato successiva alla visita del centro sportivo
+		elif(stato == 5):
+			var fifth_dialog = Dialogic.start('Scelta1_Prefabbricato')
+			add_child(fifth_dialog)
+			print("tempo scaduto, alessia morta")
+			# nessuna funzione signal perche comprende la morte
+		
+		##scelta residence successiva alla visita del centro sportivo
+		elif(stato == 6):
+			var sixth_dialog = Dialogic.start('Scelta2_Residence')
+			add_child(sixth_dialog)
+			sixth_dialog.connect("dialogic_signal",self, 'signal_residence')
+			
+			
+			
 			
 			
 			
@@ -39,7 +61,38 @@ func after_chiamata1(chiamata1):
 
 func after_chiamata2(chiamata2):
 		print("fine chiamata numero 2")
-		Global.stato_chiamata = 3
+		
+
+# in base alla decisione presa in chiamata 2, la terza chiamata puo avere 3 opzioni diverse		
+func signal_chiamata2(argument):
+		if(argument == 'residence'):
+			print("ha scelto residence")
+			Global.stato_chiamata = 3
+		if(argument == 'centrosportivo'):
+			print("ha scelto centrosportivo")
+			Global.stato_chiamata = 4
+		if(argument == 'prefabbricato'):
+			print("ha scelto prefabbricato")
+		##	Global.stato_chiamata = 5
+
+
+#sei al centro sportivo, in base alla scelta vai a prefabbricato o a residence			
+func signal_centrosportivo (arg):
+	if arg == 'centro_to_pref':
+		print("va da centro sportivo a prefabbricato")
+		Global.stato_chiamata = 5
+	elif arg =='centro_to_residence':
+		print("va da centro sportivo a residence")
+		Global.stato_chiamata = 6
+
+#sei al residence dopo il centro, in base alla scelta vai a teatro, app, o rece
+func signal_residence(arg):
+	if arg == 'residence_teatro':
+		print("va nel teatro del residence")
+	if arg == 'residence_reception':
+		print("va nella reception del residence")
+	if arg == 'residence_appartamenti':
+		print("va negli appartamenti del residence")
 
 func _on_door_mouse_exited():
 	pass # Replace with function body.
